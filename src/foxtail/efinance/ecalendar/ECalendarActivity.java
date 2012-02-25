206c103eba385f85d03a092e2797a1de6811a5e6
greenfoxtail <greenfoxtail@gmail.com>
2012-02-25 11:03:09 +0800

    优化代码 并实现向上滚屏实现返回当天

--- src/foxtail/efinance/ecalendar/ECalendarActivity.java
+++ src/foxtail/efinance/ecalendar/ECalendarActivity.java
@@ -19,13 +19,10 @@ import foxtail.util.Date;
 
 public class ECalendarActivity extends Activity implements OnGestureListener{
 	
-	//获取当前日期
     private Time t;    
     private Date nowDate;
     private Date tempDate;
-    
-    
-    
+      
     class PreAfter
     {
     	int tag;//用于循环显示viewflipper避免日历翻滚时数据发生变化，主要日历刷新比翻页快的原因导致。
@@ -38,75 +35,84 @@ public class ECalendarActivity extends Activity implements OnGestureListener{
     private PreAfter pa;
     
     private Button nowDateButton;
+    private Button dateDecButton;
+    private Button dateIncButton;
+    
     private TextView inFo;
     private TextView tv;
+    
     private DayGridView preDayTable;
     private DayGridView afterDayTable;
-    private DayGridView DayTable[]; 
-    private  ViewFlipper calFlipper;
+    private DayGridView DayTable[];
+    private ViewFlipper calFlipper;
     
     private GestureDetector mGestureDetector;
     
-    Calendar cr = new Calendar();
+    Calendar cr;
     
+    private void initialAll()
+    {
+    	cr = new Calendar();
+    	mGestureDetector = new GestureDetector(this);//滑动
+        pa = new PreAfter();
+        //----------------------------获取当前时间初始化
+        nowDate = new Date();
+        tempDate = new Date();
+        t = new Time();
+        t.setToNow();
+        int year = t.year;
+        int month = t.month+1;
+        int day = t.monthDay;
+        nowDate.setDate(year, month, day);
+        tempDate.setDate(nowDate);
+        //----------------------------转换成农历
+        cr.setSolarDate(nowDate);
+        cr.Solar2Lunar();
+        //----------------------------当月按钮，上下个月按钮初始化
+        nowDateButton = (Button)findViewById(R.id.today_id);
+        dateDecButton = (Button)findViewById(R.id.month_dec_id);
+        dateIncButton = (Button)findViewById(R.id.month_inc_id);
+        //----------------------------农历详细信息与该月日历年份与月份信息文本控件的初始化
+        inFo = (TextView)findViewById(R.id.lunar_info_id);
+        tv = (TextView)findViewById(R.id.year_month_text_id);
+        //----------------------------滑屏效果所需的两个DayGridView控件及ViewFlipper
+        preDayTable = (DayGridView)findViewById(R.id.pre_days_grid_id);
+        afterDayTable = (DayGridView)findViewById(R.id.after_days_grid_id); 
+        DayTable = new DayGridView[2];
+        DayTable[0] = preDayTable;
+        DayTable[1] = afterDayTable;
+        calFlipper = (ViewFlipper)findViewById(R.id.cal_flip_id);
+    }
+    
+    private boolean isToday()
+    {
+    	if((tempDate.getYear() == nowDate.getYear()) && (tempDate.getMonth() == nowDate.getMonth()))
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    }
     
 	 public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.ecalendar);
-	        
-	        mGestureDetector = new GestureDetector(this);//滑动
-	        pa = new PreAfter();
-	        nowDate = new Date();
-	        tempDate = new Date();
-	        t = new Time();
-	        t.setToNow();
-	        int year = t.year;
-	        int month = t.month+1;
-	        int day = t.monthDay;
-	        nowDate.setDate(year, month, day);
-	        tempDate.setDate(nowDate);
-	        
-	        cr.setSolarDate(nowDate);
-	        cr.Solar2Lunar();
-	        
-	        nowDateButton = (Button)findViewById(R.id.today_id);
-	        inFo = (TextView)findViewById(R.id.lunar_info_id);
-	        tv = (TextView)findViewById(R.id.year_month_text_id);
-	        preDayTable = (DayGridView)findViewById(R.id.pre_days_grid_id);
-	        afterDayTable = (DayGridView)findViewById(R.id.after_days_grid_id); 
-	        DayTable = new DayGridView[2];
-	        DayTable[0] = preDayTable;
-	        DayTable[1] = afterDayTable;
-	        calFlipper = (ViewFlipper)findViewById(R.id.cal_flip_id);
-	        
-	        //calFlipper.setOnTouchListener(mGestureDetector.onTouchEvent());
-	        
+	        setContentView(R.layout.ecalendar);      
+	        this.initialAll();
+	         
 	        inFo.setTextColor(Color.BLACK);
 	        inFo.setText(cr.getGanZhiYear()+"年  生肖【"+cr.getShengXiao()+"】 今天是 农历:"+cr.getLunarDate().getMonthInHanzi()+"月 "+cr.getLunarDate().getDayInHanzi());
-	        
-	        
 	        tv.setTextColor(Color.BLACK);
 	        tv.setText(Integer.toString(nowDate.getYear())+"年"+Integer.toString(nowDate.getMonth())+"月");
-	        
-	        
-	        preDayTable.setCalendarDate(nowDate);
-	        
-	        
+	               
+	        preDayTable.setCalendarDate(nowDate);      
 	        afterDayTable.setCalendarDate(nowDate);
-	        
-	        
-	        Button dateDecButton = (Button)findViewById(R.id.month_dec_id);
-	        Button dateIncButton = (Button)findViewById(R.id.month_inc_id);
-	        
-	        
 	        nowDateButton.setVisibility(View.GONE);
-	        
-	        
-	        
+	          
 	        //==============================================================================
 	        //各种监听事件啊
-	        //==============================================================================
-	        
+	        //==============================================================================    
 	        class DecOnClickListener implements Button.OnClickListener
 	        {
 	        	private Context context;
@@ -276,12 +282,14 @@ public class ECalendarActivity extends Activity implements OnGestureListener{
 		return true;
 	}
 
+	
+	private final int flipSize = 90;
 	@Override
 	public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2,
 			float arg3) {
 		// TODO Auto-generated method stub
 		Log.i("FG", "执行了滑屏");
-		if(arg0.getX()>arg1.getX())//向左移动
+		if(arg0.getX()-arg1.getX()>flipSize)//向左移动
 		{
 			if(tempDate.getMonth() + 1 >=12 )
 			{
@@ -325,7 +333,7 @@ public class ECalendarActivity extends Activity implements OnGestureListener{
 			calFlipper.showNext();
 			
 		}
-		else if(arg0.getX()<arg1.getX())
+		else if(arg0.getX()-arg1.getX()<-flipSize)
 		{
 			if(tempDate.getMonth()-1 <=0)
 			{
@@ -367,6 +375,27 @@ public class ECalendarActivity extends Activity implements OnGestureListener{
 			calFlipper.setInAnimation(this,R.layout.ani_dec_in);
 	        calFlipper.setOutAnimation(this,R.layout.ani_dec_out);
 			calFlipper.showPrevious();
+		}
+		else if((arg0.getX()-arg1.getX()<flipSize) &&
+				(arg0.getX()-arg1.getX()>-flipSize)&&
+				(arg0.getY()-arg1.getY()>2*flipSize)&& !this.isToday()
+				)
+		{
+			tempDate.setDate(nowDate);
+			tv.setText(Integer.toString(tempDate.getYear())+"年"+Integer.toString(tempDate.getMonth())+"月");
+			nowDateButton.setVisibility(View.GONE);
+			if(pa.tag == 0)
+			{
+				pa.tag = 1;
+			}
+			else
+			{
+				pa.tag = 0;
+			}
+			DayTable[pa.tag].setCalendarDate(tempDate);					
+			calFlipper.setInAnimation(this,R.layout.ani_today_in);
+	        calFlipper.setOutAnimation(this,R.layout.ani_today_out);
+			calFlipper.showNext();
 		}
 		else
 		{
